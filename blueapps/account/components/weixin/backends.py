6063818c601a8e0cdc2ff9e862ef95b5d96918fb
zhmf7408 <kido@canway.net>
2023-06-20 17:28:13 +0800

    feature 修改微信验证

--- blueapps/account/components/weixin/backends.py
+++ blueapps/account/components/weixin/backends.py
@@ -12,17 +12,26 @@ specific language governing permissions and limitations under the License.
 """
 
 import logging
+import traceback
 
+from django.conf import settings
 from django.contrib.auth.backends import ModelBackend
+from django.core.cache import caches
+from django.db import IntegrityError
 
 from blueapps.account import get_user_model
 from blueapps.account.components.weixin.weixin_utils import WechatUtils
 from blueapps.account.conf import ConfFixture
 from blueapps.account.utils.http import send
 from blueapps.core.exceptions import BlueException
+from blueapps.utils import client
 from blueking.component.shortcuts import get_client_by_user
 
 logger = logging.getLogger("component")
+
+ROLE_TYPE_ADMIN = "1"
+
+cache = caches["login_db"]
 
 
 class WeixinBackend(ModelBackend):
@@ -31,7 +40,11 @@ class WeixinBackend(ModelBackend):
         is_wechat 参数是为了使得 WeixinBackend 与其他 Backend 参数个数不同，在框架选择
         认证 backend 时，快速定位
         """
-        logger.debug("进入 WEIXIN 认证 Backend")
+        bk_token = request.COOKIES.get("bk_token", "")
+        if bk_token:
+            user = self.authenticate_user(bk_token, request)
+            if user:
+                return user
         if not code:
             return None
 
@@ -84,3 +97,98 @@ class WeixinBackend(ModelBackend):
             logger.exception(result["message"])
             raise BlueException()
         return result["data"]
+
+    def get_user_by_bk_token(self, bk_token):
+        api_params = {"bk_token": bk_token}
+        try:
+            response = send(ConfFixture.VERIFY_URL, "GET", api_params, verify=False)
+        except Exception:
+            logger.exception(u"Abnormal error in verify_bk_token...")
+            return None
+        if not response.get("result"):
+            error_msg = response.get("message", "")
+            error_data = response.get("data", "")
+            logger.error(u"Fail to verify bk_token, error={}, ret={}".format(error_msg, error_data))
+            return
+        data = response.get("data")
+        username = data.get("username")
+        user_model = get_user_model()
+        try:
+            user, _ = user_model.objects.get_or_create(username=username)
+            get_user_info_result, user_info = self.get_user_info(bk_token)
+            # 判断是否获取到用户信息,获取不到则返回None
+            if not get_user_info_result:
+                return None
+            user.set_property(key="qq", value=user_info.get("qq", ""))
+            user.set_property(key="language", value=user_info.get("language", ""))
+            user.set_property(key="time_zone", value=user_info.get("time_zone", ""))
+            user.set_property(key="role", value=user_info.get("role", ""))
+            user.set_property(key="phone", value=user_info.get("phone", ""))
+            user.set_property(key="email", value=user_info.get("email", ""))
+            user.set_property(key="wx_userid", value=user_info.get("wx_userid", ""))
+            user.set_property(key="chname", value=user_info.get("chname", ""))
+
+            # 用户如果不是管理员，则需要判断是否存在平台权限，如果有则需要加上
+            if not user.is_superuser and not user.is_staff:
+                role = user_info.get("role", "")
+                is_admin = True if str(role) == ROLE_TYPE_ADMIN else False
+                user.is_superuser = is_admin
+                user.is_staff = is_admin
+                user.save()
+            return user
+        except IntegrityError:
+            logger.exception(traceback.format_exc())
+            logger.exception(u"get_or_create UserModel fail or update_or_create UserProperty")
+            return None
+        except Exception:
+            logger.exception(traceback.format_exc())
+            logger.exception(u"Auto create & update UserModel fail")
+            return None
+
+    @staticmethod
+    def get_user_info(bk_token):
+        api_params = {"bk_token": bk_token}
+        try:
+            response = client.bk_login.get_user(api_params)
+        except Exception as e:
+            logger.exception(u"Abnormal error in get_user_info...:%s" % e)
+            return False, {}
+
+        if response.get("result") is True:
+            # 由于v1,v2的get_user存在差异,在这里屏蔽字段的差异,返回字段相同的字典
+            origin_user_info = response.get("data", "")
+            user_info = dict()
+            # v1,v2字段相同的部分
+            user_info["wx_userid"] = origin_user_info.get("wx_userid", "")
+            user_info["language"] = origin_user_info.get("language", "")
+            user_info["time_zone"] = origin_user_info.get("time_zone", "")
+            user_info["phone"] = origin_user_info.get("phone", "")
+            user_info["chname"] = origin_user_info.get("chname", "")
+            user_info["email"] = origin_user_info.get("email", "")
+            user_info["qq"] = origin_user_info.get("qq", "")
+            # v2版本特有的字段
+            if settings.DEFAULT_BK_API_VER == "v2":
+                user_info["username"] = origin_user_info.get("bk_username", "")
+                user_info["role"] = origin_user_info.get("bk_role", "")
+            # v1版本特有的字段
+            elif settings.DEFAULT_BK_API_VER == "":
+                user_info["username"] = origin_user_info.get("username", "")
+                user_info["role"] = origin_user_info.get("role", "")
+            return True, user_info
+        else:
+            error_msg = response.get("message", "")
+            error_data = response.get("data", "")
+            logger.error(u"Failed to Get User Info: error={err}, ret={ret}".format(err=error_msg, ret=error_data,))
+            return False, {}
+
+    def authenticate_user(self, bk_token, request):
+        session_key = request.session.session_key
+        if session_key:
+            # 确认 cookie 中的 ticket 和 cache 中的是否一致
+            cache_session = cache.get(session_key)
+            is_match = cache_session and bk_token == cache_session.get("bk_token")
+            if is_match and request.user.is_authenticated:
+                return 0
+        user = self.get_user_by_bk_token(bk_token)
+
+        return user
--- blueapps/account/components/weixin/middlewares.py
+++ blueapps/account/components/weixin/middlewares.py
@@ -1,28 +1,18 @@
 import logging
 import random
 import time
-import traceback
 
 from django.conf import settings
 from django.contrib import auth
-from django.core.cache import caches
-from django.db import IntegrityError
 from django.http import JsonResponse
 from django.utils.deprecation import MiddlewareMixin
 
-from blueapps.account import get_user_model
 from blueapps.account.components.weixin.forms import WeixinAuthenticationForm
 from blueapps.account.conf import ConfFixture
 from blueapps.account.handlers.response import ResponseHandler
-from blueapps.account.utils.http import send
-from blueapps.utils import client
 from utils.token import generate_bk_token, set_bk_token_to_open_pass_db
 
 logger = logging.getLogger("component")
-
-ROLE_TYPE_ADMIN = "1"
-
-cache = caches["login_db"]
 
 
 class WeixinLoginRequiredMiddleware(MiddlewareMixin):
@@ -39,26 +29,15 @@ class WeixinLoginRequiredMiddleware(MiddlewareMixin):
         login_exempt = getattr(view, "login_exempt", False)
         user = 0
         if not (login_exempt or request.user.is_authenticated):
-            bk_token = request.COOKIES.get("bk_token", "")
-            if bk_token:
-                user = self.authenticate_user(bk_token, request)
-            else:
-                form = WeixinAuthenticationForm(request.GET)
-                if form.is_valid():
-                    code = form.cleaned_data["code"]
-                    state = form.cleaned_data["state"]
+            form = WeixinAuthenticationForm(request.GET)
+            if form.is_valid():
+                code = form.cleaned_data["code"]
+                state = form.cleaned_data["state"]
 
-                    if self.valid_state(request, state):
-                        user = auth.authenticate(request=request, code=code, is_wechat=True)
-                        if user == 0:
-                            return JsonResponse({"result": False, "message": "用户验证失败!"})
-                        if user and user.username != request.user.username:
-                            auth.login(request, user)
-                        if request.user.is_authenticated:
-                            # 登录成功，确认登陆正常后退出
-                            return None
-                else:
-                    logger.error("微信请求链接，未检测到微信验证码，url：{}，params：{}".format(request.path_info, request.GET))
+                if self.valid_state(request, state):
+                    user = auth.authenticate(request=request, code=code, is_wechat=True)
+            else:
+                logger.error("微信请求链接，未检测到微信验证码，url：{}，params：{}".format(request.path_info, request.GET))
             if user == 0:
                 return JsonResponse({"result": False, "message": "用户验证失败!"})
             if user and user.username != request.user.username:
@@ -116,98 +95,3 @@ class WeixinLoginRequiredMiddleware(MiddlewareMixin):
         request.session["WEIXIN_OAUTH_STATE"] = None
         request.session["WEIXIN_OAUTH_STATE_TIMESTAMP"] = None
         return True
-
-    def get_user(self, bk_token):
-        api_params = {"bk_token": bk_token}
-        try:
-            response = send(ConfFixture.VERIFY_URL, "GET", api_params, verify=False)
-        except Exception:
-            logger.exception(u"Abnormal error in verify_bk_token...")
-            return None
-        if not response.get("result"):
-            error_msg = response.get("message", "")
-            error_data = response.get("data", "")
-            logger.error(u"Fail to verify bk_token, error={}, ret={}".format(error_msg, error_data))
-            return
-        data = response.get("data")
-        username = data.get("username")
-        user_model = get_user_model()
-        try:
-            user, _ = user_model.objects.get_or_create(username=username)
-            get_user_info_result, user_info = self.get_user_info(bk_token)
-            # 判断是否获取到用户信息,获取不到则返回None
-            if not get_user_info_result:
-                return None
-            user.set_property(key="qq", value=user_info.get("qq", ""))
-            user.set_property(key="language", value=user_info.get("language", ""))
-            user.set_property(key="time_zone", value=user_info.get("time_zone", ""))
-            user.set_property(key="role", value=user_info.get("role", ""))
-            user.set_property(key="phone", value=user_info.get("phone", ""))
-            user.set_property(key="email", value=user_info.get("email", ""))
-            user.set_property(key="wx_userid", value=user_info.get("wx_userid", ""))
-            user.set_property(key="chname", value=user_info.get("chname", ""))
-
-            # 用户如果不是管理员，则需要判断是否存在平台权限，如果有则需要加上
-            if not user.is_superuser and not user.is_staff:
-                role = user_info.get("role", "")
-                is_admin = True if str(role) == ROLE_TYPE_ADMIN else False
-                user.is_superuser = is_admin
-                user.is_staff = is_admin
-                user.save()
-            return user
-        except IntegrityError:
-            logger.exception(traceback.format_exc())
-            logger.exception(u"get_or_create UserModel fail or update_or_create UserProperty")
-            return None
-        except Exception:
-            logger.exception(traceback.format_exc())
-            logger.exception(u"Auto create & update UserModel fail")
-            return None
-
-    @staticmethod
-    def get_user_info(bk_token):
-        api_params = {"bk_token": bk_token}
-        try:
-            response = client.bk_login.get_user(api_params)
-        except Exception as e:
-            logger.exception(u"Abnormal error in get_user_info...:%s" % e)
-            return False, {}
-
-        if response.get("result") is True:
-            # 由于v1,v2的get_user存在差异,在这里屏蔽字段的差异,返回字段相同的字典
-            origin_user_info = response.get("data", "")
-            user_info = dict()
-            # v1,v2字段相同的部分
-            user_info["wx_userid"] = origin_user_info.get("wx_userid", "")
-            user_info["language"] = origin_user_info.get("language", "")
-            user_info["time_zone"] = origin_user_info.get("time_zone", "")
-            user_info["phone"] = origin_user_info.get("phone", "")
-            user_info["chname"] = origin_user_info.get("chname", "")
-            user_info["email"] = origin_user_info.get("email", "")
-            user_info["qq"] = origin_user_info.get("qq", "")
-            # v2版本特有的字段
-            if settings.DEFAULT_BK_API_VER == "v2":
-                user_info["username"] = origin_user_info.get("bk_username", "")
-                user_info["role"] = origin_user_info.get("bk_role", "")
-            # v1版本特有的字段
-            elif settings.DEFAULT_BK_API_VER == "":
-                user_info["username"] = origin_user_info.get("username", "")
-                user_info["role"] = origin_user_info.get("role", "")
-            return True, user_info
-        else:
-            error_msg = response.get("message", "")
-            error_data = response.get("data", "")
-            logger.error(u"Failed to Get User Info: error={err}, ret={ret}".format(err=error_msg, ret=error_data,))
-            return False, {}
-
-    def authenticate_user(self, bk_token, request):
-        session_key = request.session.session_key
-        if session_key:
-            # 确认 cookie 中的 ticket 和 cache 中的是否一致
-            cache_session = cache.get(session_key)
-            is_match = cache_session and bk_token == cache_session.get("bk_token")
-            if is_match and request.user.is_authenticated:
-                return 0
-        user = self.get_user(bk_token)
-
-        return user
