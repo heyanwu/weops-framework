e62b33fe12b9591f14131390203d737bc1c7c90e
zhmf7408 <kido@canway.net>
2023-09-08 14:05:37 +0800

    feature ot判断前置条件

--- settings.py
+++ settings.py
@@ -12,12 +12,15 @@ specific language governing permissions and limitations under the License.
 """
 
 import os
+import sys
 
 """
 请不要修改该文件
 如果你需要对settings里的内容做修改，config/default.py 文件中 添加即可
 如有任何疑问，请联系 【蓝鲸助手】
 """
+if not hasattr(sys, "argv"):
+    sys.argv = [""]
 
 # V3判断环境的环境变量为BKPAAS_ENVIRONMENT
 if "BKPAAS_ENVIRONMENT" in os.environ:
@@ -25,11 +28,7 @@ if "BKPAAS_ENVIRONMENT" in os.environ:
 # V2判断环境的环境变量为BK_ENV
 else:
     PAAS_V2_ENVIRONMENT = os.environ.get("BK_ENV", "development")
-    ENVIRONMENT = {
-        "development": "dev",
-        "testing": "stag",
-        "production": "prod",
-    }.get(PAAS_V2_ENVIRONMENT)
+    ENVIRONMENT = {"development": "dev", "testing": "stag", "production": "prod"}.get(PAAS_V2_ENVIRONMENT)
 DJANGO_CONF_MODULE = "config.{env}".format(env=ENVIRONMENT)
 
 try:
@@ -46,7 +45,7 @@ CELERY_IMPORTS = locals()["CELERY_IMPORTS"]
 MIDDLEWARE = locals()["MIDDLEWARE"]
 
 try:
-    __module = __import__(f"home_application.config", globals(), locals(), ["*"])
+    __module = __import__("home_application.config", globals(), locals(), ["*"])
 except ImportError:
     pass
 else:
